875e018fd232008d53143810884ff49cbf0b9959
fancn21th <fancn21th@gmail.com>
2022-11-24 17:20:28 +0800

    Link Component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,25 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <ul>
-      <li>
-        <a href="/">HOME</a>
-      </li>
-      <li>
-        <a href="/about">ABOUT</a>
-      </li>
-    </ul>
+    <>
+      <ul>
+        <li>
+          <a href="/">HOME</a>
+        </li>
+        <li>
+          <a href="/about">ABOUT</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="/">Home with Link</Link>
+        </li>
+        <li>
+          <Link href="/about">About with Link</Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
