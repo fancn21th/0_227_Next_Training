cbdd93411cea0d0a1ca9037a74154720990e11ba
fancn21th <fancn21th@gmail.com>
2022-11-24 17:27:49 +0800

    static

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <a href="/todos.json">todos</a>
+    </>
+  );
 };
 
 export default Home;
