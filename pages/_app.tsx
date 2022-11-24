9c7522b191607f59865a01c56ea144e1bfa30b1d
fancn21th <fancn21th@gmail.com>
2022-11-24 17:32:07 +0800

    head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Header from "../components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   console.log("App is running");
   return (
     <div>
+      <Head>
+        <title>Welcome to Next Training From Fanyjb</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       {/* The Component is Page Component */}
       <Header />
       <Component {...pageProps} />
