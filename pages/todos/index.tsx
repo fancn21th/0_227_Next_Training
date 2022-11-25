e94828117021379d4d35bc12bc254fb35d292364
fancn21th <fancn21th@gmail.com>
2022-11-25 12:11:20 +0800

    ssr

---
+++ pages/todos/index.tsx
@@ -0,0 +1,39 @@
+import type { GetStaticProps } from "next";
+
+type Props = {
+  todos: Array<{
+    id: number;
+    title: string;
+  }>;
+};
+
+const Todos = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map(({ title, id }) => (
+        <li>
+          {id} : {title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const res = await fetch("http://localhost:3000/todos.json");
+
+  const todos = await res.json();
+
+  console.log({ todos });
+
+  return {
+    props: {
+      todos,
+    },
+  };
+};
